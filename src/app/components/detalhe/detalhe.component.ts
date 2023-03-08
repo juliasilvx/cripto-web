97173fccd417b05161a1fd9dfc07669e1961c3cf
Sala 18A <sala18a@ceines.com.br>
2023-03-07 21:09:56 -0300

    0703

--- src/app/components/detalhe/detalhe.component.ts
+++ src/app/components/detalhe/detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Moeda } from 'src/app/models/moeda';
 import { DetalheService } from './service/detalhe.service';
 
@@ -8,20 +9,23 @@ import { DetalheService } from './service/detalhe.service';
   styleUrls: ['./detalhe.component.css']
 })
 export class DetalheComponent implements OnInit {
+  
   moedaDetalheHtml = {} as Moeda;
 
-  constructor(private service: DetalheService) { }
-
+  constructor(private service: DetalheService,
+              private activatedRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
-    
-    var id = 1;
+    debugger;
 
-    this.service.obterMoedaDetalhe(id).subscribe((moedaDetalhe: Moeda) => {
-      this.moedaDetalheHtml = moedaDetalhe;
-  })
+    var idMoeda = parseInt(this.activatedRouter.snapshot.paramMap.get('id')!)
 
+    this.service
+      .obterMoedaDetalhe(idMoeda)
+      .subscribe((moedaDetalhe: Moeda) => {
+
+        this.moedaDetalheHtml = moedaDetalhe
+
+      })
   }
-
-  
 }
--- src/app/components/listagem/listagem.component.ts
+++ src/app/components/listagem/listagem.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { Moeda } from 'src/app/models/moeda';
 import { ListagemService } from './service/listagem.service';
 
@@ -9,34 +9,32 @@ import { ListagemService } from './service/listagem.service';
   styleUrls: ['./listagem.component.css']
 })
 
-
 /* O implements On Init diz para o meu
 componente que ele tem uma classe de 
 inicialização e que ele deve passar por 
 ela antes de executar qual quer outra coisa */
-export class ListagemComponent implements OnInit{
+export class ListagemComponent implements OnInit {
 
-  id:number = 14;
+  id: number = 14;
   moedasExibirNoHtml: Moeda[] = [];
-  constructor(private moedaService: ListagemService, private router: Router){
-
-  }
+  constructor(private moedaService: ListagemService,
+              private router: Router) {}
   /*Quando inicar, ou seja, tudo que está dentro desta função será executado primeiro*/
   ngOnInit() {
 
-  /* Indo no serviço obter moedas e aguardando o retorno da api com o subscribe que recebe uma função com o parametro do tipo retorno*/ 
-  this.moedaService.obterMoedas().subscribe((moedasVindoDaApi: Moeda[]) => {
+    /* Indo no serviço obter moedas e aguardando o retorno da api com o subscribe que recebe uma função com o parametro do tipo retorno*/
+    this.moedaService.obterMoedas().subscribe((moedasVindoDaApi: Moeda[]) => {
 
-    this.moedasExibirNoHtml = moedasVindoDaApi
+      this.moedasExibirNoHtml = moedasVindoDaApi
 
-  })
-}
+    })
+  }
 
-detalhar(id:number){
+  detalhar(id: number) {
 
-  //redirecionamento p/ alguma rota da aplicação//
-    this.router.navigateByUrl(`detalhar/${id}`)
+    debugger;
+    this.router.navigateByUrl(`/detalhar/${id}`)
 
-}
+  }
 
 }
